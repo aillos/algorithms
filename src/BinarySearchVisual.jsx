ca05e06d46c1ebf3d3318d22d510f4c83a8b0279
Andreas <andreasssolli@gmail.com>
2024-02-02 23:28:21 +0100

    Fix to alot of css

--- src/BinarySearchVisual.jsx
+++ src/BinarySearchVisual.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {useState} from "react";
 import {BinarySearch} from "./BinarySearch.jsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 function BinarySearchVisual() {
     const [inputArray, setInputArray] = useState('');
@@ -11,6 +13,7 @@ function BinarySearchVisual() {
     const [currentEnd, setCurrentEnd] = useState(null);
     const [currentCount, setCurrentCount] = useState(null);
     const [resultsText, setResultsText] = useState('');
+    const [resultNumber, setResultNumber] = useState(null);
 
     const handleInputArrayChange = (event) => {
         setInputArray(event.target.value);
@@ -23,10 +26,11 @@ function BinarySearchVisual() {
     const handleSubmit = () => {
         const array = inputArray.split(',').map(Number);
         const target = Number(inputTarget);
+        setResultsText('');
+        setResultNumber(null);
         if (array.some(isNaN)) {
             alert('Invalid input. Please enter a comma-separated list of numbers.');
         } else {
-            console.log(array);
             const boxes = array.map((value, id) => ({id, value}));
             setBoxesArray(boxes);
             BinarySearch(array, target, (start, mid, end, count) => {
@@ -38,7 +42,8 @@ function BinarySearchVisual() {
                 if (result === -1) {
                     setResultsText('Number not found');
                 } else {
-                    setResultsText('Number found at index ' + result);
+                    setResultsText('Number found at index ');
+                    setResultNumber(result);
                 }
             });
         }
@@ -54,44 +59,72 @@ function BinarySearchVisual() {
             </div>
 
             <div className={"input"}>
-                Sample input arrays to test:
+                <div id={"binaryBox"}>
+                <div className={"label"}>
+                    Range
+                </div>
                 <div className={"inputButtonDiv"}>
                     <div className={"sampleInput"}>
-                        <button className={"inputButton"} onClick={() => setInputArray(Array.from({length: 10}, (_, i) => i + 1).join(','))}>10</button>
+                        <button className={"inputButton"}
+                                onClick={() => setInputArray(Array.from({length: 50}, (_, i) => i + 1).join(','))}>50
+                        </button>
                     </div>
                     <div className={"sampleInput"}>
-                        <button className={"inputButton"} onClick={() => setInputArray(Array.from({length: 50}, (_, i) => i + 1).join(','))}>100</button>
+                        <button className={"inputButton"}
+                                onClick={() => setInputArray(Array.from({length: 150}, (_, i) => i + 1).join(','))}>150
+                        </button>
                     </div>
                     <div className={"sampleInput"}>
-                        <button className={"inputButton"} onClick={() => setInputArray(Array.from({length: 100}, (_, i) => i + 1).join(','))}>100</button>
+                        <input type={"number"} className={"inputBox customBinaryArray"}
+                               onBlur={(e) => setInputArray(Array.from({length: Number(e.target.value)}, (_, i) => i + 1).join(','))}
+                               onKeyDown={(e) => {
+                                   if (e.key === 'Enter') {
+                                       e.preventDefault();
+                                       setInputArray(Array.from({length: Number(e.target.value)}, (_, i) => i + 1).join(','));
+                                   }
+                               }} placeholder={"250"}>
+                        </input>
                     </div>
-                    <div className={"sampleInput"}>
-                        <button className={"inputButton"} onClick={() => setInputArray(Array.from({length: 200}, (_, i) => i + 1).join(','))}>200</button>
+                </div>
+                    <div className={"sampleInput targetButtonDiv"}>
+                        <input id={"binaryArray"} className={"inputBox"} type="text" value={inputArray}
+                               onChange={handleInputArrayChange} placeholder={"1,2,3,4,5,6"}/>
                     </div>
                 </div>
-                Current input array:
-                <div className={"sampleInput"}>
-                    <input type="text" value={inputArray} onChange={handleInputArrayChange} placeholder={"1,2,3,4,5,6"}/>
+                <div id={"binaryBox"}>
+                    <div className={"labelTarget"}>
+                    Target
                 </div>
-                <br />
-                Target number:
-                <div className={"sampleInput"}>
-                <input type={"number"} value={inputTarget} onChange={handleInputTargetChange} placeholder={"3"}/>
+                    <div className={"inputButtonDiv targetButtonDiv"}>
+                        <div className={"sampleInput"}>
+                            <button className={"inputButton targetButton"}
+                                    onClick={() => setInputTarget(inputArray.split(',')[0])}>Start
+                            </button>
+                        </div>
+                        <div className={"sampleInput"}>
+                            <button className={"inputButton targetButton"}
+                                    onClick={() => setInputTarget(inputArray.split(',').pop())}>End
+                            </button>
+                        </div>
+                    </div>
+                    <div className={"sampleInput binarySubmit targetButtonDiv"}>
+                        <input className={"inputBox"} id={"binaryNumber"} type={"number"} value={inputTarget}
+                               onChange={handleInputTargetChange} placeholder={"3"}/>
+                        <button id={"binarySubmit"} onClick={handleSubmit}><FontAwesomeIcon icon={faSearch}/></button>
                 </div>
-                <button onClick={handleSubmit}>Search</button>
+            </div>
             </div>
             <br/>
             Current amount of loops: <b>{currentCount}</b>
-            <br />
-            <b>Result:</b> {resultsText}
+            <br/>
+            <b>Result:</b> {resultsText} <b>{resultNumber}</b>
             <div id={"legend"}>
-                <div className={"legendBoxes"}>Start = <div className={"box start legendBox"}> </div></div>
-                <div className={"legendBoxes"}>Mid = <div className={"box mid legendBox"}> </div> </div>
-                <div className={"legendBoxes"}>End = <div className={"box end legendBox"}> </div> </div>
+                <div className={"legendBoxes"}>Range: <div className={"box within legendBox"}></div></div>
+                <div className={"legendBoxes"}>Mid: <div className={"box mid legendBox"}> </div> </div>
             </div>
             <div id={"boxes"}>
                 {boxesArray.map((box, index) =>
-                    <div key={box.id} className={`box ${index === currentStart ? 'start' : ''} ${index === currentMid ? 'mid' : ''} ${index === currentEnd ? 'end' : ''}`}>{box.value}</div>
+                    <div key={box.id} className={`box ${index === currentStart ? 'start' : ''} ${index === currentMid ? 'mid' : ''} ${index === currentEnd ? 'end' : ''} ${index < currentEnd && index > currentStart? 'within' : ''}`}>{box.value}</div>
                 )}
             </div>
         </>
